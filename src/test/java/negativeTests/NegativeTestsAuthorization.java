515d96a692830fa06f8977743eeb2389421b2dbd
Brevno78 <168448563+Brevno78@users.noreply.github.com>
2026-08-19 22:34:50 +0300

    Обновил

--- src/test/java/negativeTests/NegativeTestsAuthorization.java
+++ src/test/java/negativeTests/NegativeTestsAuthorization.java
@@ -4,6 +4,8 @@ import pages.BasePage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.sql.SQLOutput;
+
 public class NegativeTestsAuthorization extends BasePage {
     WebDriver driver;
     WebDriverWait wait;
@@ -11,5 +13,8 @@ public class NegativeTestsAuthorization extends BasePage {
     public NegativeTestsAuthorization(WebDriver driver, WebDriverWait wait){
         this.driver = driver;
         this.wait = wait;
+    }
+    public void MyTest(){
+        System.out.println("Hello World");
     }
 }
