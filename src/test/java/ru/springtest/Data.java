1ab3e70c5652c870ee65c58048b19618d1fd74c2
Brevno78 <168448563+Brevno78@users.noreply.github.com>
2026-08-19 22:47:26 +0300

    Обновил

--- src/test/java/ru/springtest/Data.java
+++ src/test/java/ru/springtest/Data.java
@@ -5,4 +5,7 @@ public class Data {
     public static final Person person_Test_1 = new Person("firstname", "lastname", "address", "city", 121342345);
     public static final Person person_Test_2 = new Person("firstname", "lastname", "address", "city", 1213);
     public static final Person testDev = new Person("firstname", "lastname", "address", "city", 123124 );
+    public void test(){
+
+    }
 }
